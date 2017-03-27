ad39f0108cb50212fcb401eb63f975a4a54e2590
Istvan Burbank <ipb7@cornell.edu>
2017-03-27 10:36:20 -0400

    Small work on PI controller

--- src/X_Axis_Subsystem/PI_Controller.vhd
+++ src/X_Axis_Subsystem/PI_Controller.vhd
@@ -1,17 +1,17 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all ;
 ENTITY PI_Controller IS
-	PORT ( error : IN INTEGER ;
-		  control : OUT INTEGER;
-			   clk : in std_logic;
-          reset : in std_logic) ;
+  PORT ( error : IN INTEGER ;
+         control : OUT INTEGER;
+         clk : in std_logic;
+         reset : in std_logic) ;
 END PI_Controller;
 
 architecture Behavioral of PI_Controller is
-	signal u1: std_logic_vector(15 downto 0);
-	constant k1: std_logic_vector( 6 downto 0 ):="1101011";
-	begin
-	process( clk)
-	begin
-	end process;
+  signal u1: std_logic_vector(15 downto 0);
+  constant k1: std_logic_vector( 6 downto 0 ) := "1101011";
+begin
+  process( clk)
+  begin
+  end process;
 end Behavioral;
